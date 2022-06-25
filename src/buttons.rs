4282d0547f5f0335d298668b32762fbee623ee4f
Mihail Antonov <antonovma@outlook.com>
2022-06-25 14:07:22 +0400

    two horizontal buttons

---
+++ src/buttons.rs
@@ -0,0 +1,77 @@
+extern crate gtk;
+use gtk::*;
+
+pub struct Application {
+    pub window: Window,
+    pub header: Header,
+}
+
+pub struct Header {
+    pub container: HeaderBar,
+}
+
+pub struct Content {
+    pub container: Box,
+}
+
+impl Application {
+    pub fn new() -> Application {
+        let window = Window::new(WindowType::Toplevel);
+        // header bar and it's content
+        let header = Header::new();
+        let content = Content::new();
+
+        // widget's title
+        window.set_titlebar(&header.container);
+        // window's title
+        window.set_title("Just an app");
+        // manager's class
+        window.set_wmclass("app-name", "Just an app");
+        // app's icon
+        Window::set_default_icon_name("icon's name");
+
+        window.add(&content.container);
+
+        // Push button = close app
+        window.connect_delete_event(move |_, _| {
+            main_quit();
+            Inhibit(false)
+        });
+
+        // return app's state
+        Application { window, header }
+    }
+}
+
+impl Header {
+    fn new() -> Header {
+        // create widget on the header
+        let container = HeaderBar::new();
+        // add app's name on the header bar
+        container.set_title("Just an app");
+        // make buttons active
+        container.set_show_close_button(true);
+        
+        Header {
+            container,
+            // button_1,
+        }
+    }
+}
+
+impl Content {
+    fn new() -> Content {
+        let container = Box::new(Orientation::Horizontal, 0);
+
+        let button_1 = Button::new_with_label("Button №1");
+        let button_2 = Button::new_with_label("Button №2");
+        container.pack_start(&button_1, true, false, 0);
+        container.pack_start(&button_2, true, false, 0);
+        button_1.set_halign(Align::Start);
+        button_2.set_halign(Align::Center);
+
+        Content {
+            container,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,17 @@
 extern crate gtk;
-
 use gtk::*;
 
+mod buttons;
+
 fn main() {
-    // Инициализация GTK.
+    // start GTK
     if gtk::init().is_err() {
         eprintln!("GTK failed");
         return;
     }
 
-    // UI state
-    let app = Application::new();
+    let app = buttons::Application::new();   // UI state
 
-    // make widgets visible
-    app.window.show_all();
-
-    // start GTK loop
-    gtk::main();
-}
-
-pub struct Application {
-    pub window: Window,
-    pub header: Header,
-}
-
-pub struct Header {
-    pub container: HeaderBar,
-    pub button_1: Button, // add button on the header
-}
-
-impl Application {
-    fn new() -> Application {
-        let window = Window::new(WindowType::Toplevel);
-        // header bar and it's content
-        let header = Header::new();
-
-        // widget's title
-        window.set_titlebar(&header.container);
-        // window's title
-        window.set_title("Just an app");
-        // manager's class
-        window.set_wmclass("simple-gtk", "Just an app");
-        // app's icon
-        Window::set_default_icon_name("icon's name");
-
-        // Push button = close app
-        window.connect_delete_event(move |_, _| {
-            main_quit();
-            Inhibit(false)
-        });
-
-        // return app's state
-        Application { window, header }
-    }
-}
-
-impl Header {
-    fn new() -> Header {
-        // create widget on the header
-        let container = HeaderBar::new();
-        // add app's name on the header bar
-        container.set_title("Just an app");
-        // make buttons active
-        container.set_show_close_button(true);
-
-        let button_1 = Button::new_with_label("Button №1"); // DOES NOTHING
-        button_1
-            .get_style_context()
-            .map(|c| c.add_class("no action"));
-
-        button_1.connect_clicked(move |button_1| {
-            button_1.set_label("back off!");
-        });
-
-        container.pack_start(&button_1);
-
-        // return header's state
-        Header {
-            container,
-            button_1,
-        }
-    }
+    app.window.show_all();  // make widgets visible
+    gtk::main();            // start GTK loop
 }
