6abe5a62e090875dffadfc3604a774c2e0a9a64e
Mihail Antonov <antonovma@outlook.com>
2022-07-17 16:10:33 +0400

    screenshot

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Using external markdown file grid.ui for building keyboard.
 Button labeled button0 is sliding from side to side after each click.
 
 Buttons 1-9 change their labels to Roman Numbers after first click.
+
+----------
+
+How to add Entry?
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ mod buttons;
 fn main() {
     let application =
         gtk::Application::new(Some("com.github.gtk-rs.examples.grid"), Default::default());
-    // Some("com.github.gtk-rs.examples.grid") - application_id
 
     application.connect_activate(buttons::build_ui);
     application.run();
